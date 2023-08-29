bd9e92e981d5064e1cf62e4e0037ddd06d72cfd6
Alyxia Sother <alyxia@riseup.net>
2023-08-29 22:03:36 +0200

    [*] Add logto implementation

--- resources/site/deps.js
+++ resources/site/deps.js
@@ -2,4 +2,17 @@ ig.module('site.deps').defines(async () => {
   const ca = await import('https://unpkg.com/crossani@1.3.3/dist/esm/index.js');
   ig.EASE = ca.EASE;
   ig.JUMP = ca.JUMP;
+
+  const logto = await import("https://cdn.jsdelivr.net/npm/@logto/browser@2.1.1/+esm");
+  window.logto = logto;
+
+  const logtoClient = new logto.default({
+    endpoint: "https://auth.fyralabs.com",
+    appId: "eewbfi2u6pziz9cu46bat"
+  })
+  window.logtoClient = logtoClient;
+
+  if (window.location.pathname === "/callback.html") {
+    await logtoClient.handleSignInCallback(window.location.href);
+  }
 });
