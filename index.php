d4fd62f866bb8d22c02cb9a881cb2024f339bb7f
Alyxia Sother <alyxia@riseup.net>
2023-08-30 01:15:13 +0200

    [index.php] Rewrite for new structure

--- index.php
+++ index.php
@@ -11,36 +11,36 @@ $client = new LogtoClient(
   new LogtoConfig(
     endpoint: "https://auth.fyralabs.com",
     appId: "eewbfi2u6pziz9cu46bat",
-    appSecret: "XXX",
   )
 );
 
-$router->get('/sign-in', function() use ($router, $client) {
-    header("Location: " . $client->signIn("http://localhost:8080/callback"));
+$router->get('/', function() {
+   echo file_get_contents("views/index.html");
+});
+$router->get('/cms', function() use ($client) {
+    if (!$client->isAuthenticated()) {
+        header('Location: /');
+    }
+    echo file_get_contents("views/cms.html");
 });
 
-$router->get('/callback', function () use ($client) {
+$router->get('/sign-in', function() use ($client) {
+    header("Location: {$client->signIn("http://{$_SERVER["HTTP_HOST"]}/callback")}");
+});
+
+$router->get('/callback', function() use ($client) {
   try {
     $client->handleSignInCallback();
   } catch (LogtoException $exception) {
     return $exception;
   }
-  header("Location: /"); // Redirect the user to the home page after a successful sign-in
-});
 
-$router->get('/', function () use ($client) {
-  if ($client->isAuthenticated() === false) {
-    echo "Not authenticated <a href='/sign-in'>Sign in</a>";
+  $user = $client->fetchUserInfo();
+  if ($user->sub === "igd4qm8vr5kc") {
+      header('Location: /cms');
+  } else {
+      header('Location: /');
   }
-
-  echo (
-    // Get local ID token claims
-    var_dump($client->getIdTokenClaims())
-    . "<br>" .
-    // Fetch user info from Logto userinfo endpoint
-    var_dump($client->fetchUserInfo())
-    . "</pre><br><a href='/sign-out'>Sign out</a>"
-  );
 });
 
 $router->run();
