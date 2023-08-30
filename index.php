8fff2230cec275c96cd894533538d554faa7ef17
Alyxia Sother <alyxia@riseup.net>
2023-08-30 21:25:37 +0200

    [index.php] Rewrite to use Illuminate router

--- index.php
+++ index.php
@@ -1,44 +1,20 @@
 <?php
 require __DIR__ . "/vendor/autoload.php";
 
-use Bramus\Router\Router;
-use Logto\Sdk\LogtoClient;
-use Logto\Sdk\LogtoConfig;
+use Illuminate\Container\Container;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Router;
 
-$router = new Router();
-$client = new LogtoClient(
-  new LogtoConfig(
-    endpoint: "https://auth.fyralabs.com",
-    appId: "eewbfi2u6pziz9cu46bat",
-  )
-);
-
-$router->get('/', function() {
-   echo file_get_contents("views/index.html");
-});
-$router->get('/cms', function() use ($client) {
-    if (!$client->isAuthenticated()) {
-        header('Location: /');
-    }
-    echo file_get_contents("views/cms.html");
+$container = new Container();
+$events = new Dispatcher();
+$request = Request::capture();
+$container->instance('Illuminate\Http\Request', $request);
+$container->singleton('Illuminate\Routing\Contracts\CallableDispatcher', function() use ($container) {
+    return new Illuminate\Routing\CallableDispatcher($container);
 });
 
-$router->get('/sign-in', function() use ($client) {
-    header("Location: {$client->signIn("https://{$_SERVER["HTTP_HOST"]}/callback")}");
-});
-
-$router->get('/callback', function() use ($client) {
-  // required because Logto thinks it's a good idea to check for things like
-  // PATH_INFO that may not even exist
-  $_SERVER['PATH_INFO'] = '/callback';
-  $client->handleSignInCallback();
-
-  $user = $client->fetchUserInfo();
-  if ($user->sub === "igd4qm8vr5kc") {
-      header('Location: /cms');
-  } else {
-      header('Location: /');
-  }
-});
-
-$router->run();
+$router = new Router($events, $container);
+require_once 'routes.php';
+$response = $router->dispatch($request);
+$response->send();
