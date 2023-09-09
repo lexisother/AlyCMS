55e7c1a64a869ef84f11c09306b60a3c1ef91f75
Alyxia Sother <alyxia@riseup.net>
2023-09-09 19:56:04 +0200

    [Bootstrap/Twig] Add the Markdown extension

--- src/Bootstrap/Twig.php
+++ src/Bootstrap/Twig.php
@@ -3,9 +3,28 @@
 namespace App\Bootstrap;
 
 use App\Application;
+use League\CommonMark\GithubFlavoredMarkdownConverter;
 use Twig\Environment;
+use Twig\Extra\Markdown\{MarkdownExtension,MarkdownInterface,MarkdownRuntime};
 use Twig\Extension\DebugExtension;
 use Twig\Loader\FilesystemLoader;
+use Twig\RuntimeLoader\RuntimeLoaderInterface;
+
+// Twig's Markdown package doesn't support enabling GFM when using League's package, so we'll have to wrap that ourselves.
+class GFM implements MarkdownInterface
+{
+    private GithubFlavoredMarkdownConverter $converter;
+
+    public function __construct()
+    {
+        $this->converter = new GithubFlavoredMarkdownConverter();
+    }
+
+    public function convert(string $body): string
+    {
+        return $this->converter->convert($body);
+    }
+}
 
 class Twig {
     public function bootstrap(Application $app) {
@@ -19,6 +38,14 @@ class Twig {
             ]);
 
             $env->addExtension(new DebugExtension());
+            $env->addExtension(new MarkdownExtension());
+            $env->addRuntimeLoader(new class implements RuntimeLoaderInterface {
+                public function load($class) {
+                    if (MarkdownRuntime::class === $class) {
+                        return new MarkdownRuntime(new GFM());
+                    }
+                }
+            });
 
             return $env;
         });
