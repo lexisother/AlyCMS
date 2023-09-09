45540e3354c61b17736b77a64081443547c8c175
Alyxia Sother <alyxia@riseup.net>
2023-09-09 19:51:03 +0200

    [Bootstrap/Twig] Add the debug extension

--- src/Bootstrap/Twig.php
+++ src/Bootstrap/Twig.php
@@ -4,17 +4,23 @@ namespace App\Bootstrap;
 
 use App\Application;
 use Twig\Environment;
+use Twig\Extension\DebugExtension;
 use Twig\Loader\FilesystemLoader;
 
 class Twig {
     public function bootstrap(Application $app) {
         $app->singleton('twig', function($app) {
             $loader = new FilesystemLoader($app->joinPaths($app->basePath, 'views'));
-            return new Environment($loader, [
+            $env = new Environment($loader, [
+                'debug' => $app['config']['app.env'] != 'production',
                 'cache' => $app['config']['app.env'] == 'production'
                     ? $app->joinPaths($app->basePath, '.cache')
                     : false,
             ]);
+
+            $env->addExtension(new DebugExtension());
+
+            return $env;
         });
     }
 }
