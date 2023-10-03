50f947ed0bf001cc56e93440833b45a903946dcf
Alyxia Sother <alyxia@riseup.net>
2023-10-04 01:00:40 +0200

    [Bootstrap/Mastodon] Provide empty defaults

--- src/Bootstrap/Mastodon.php
+++ src/Bootstrap/Mastodon.php
@@ -7,7 +7,7 @@ use App\Helper\MastodonAPI;
 
 class Mastodon {
     public function bootstrap(Application $app) {
-        $client = new MastodonAPI(env('MASTODON_API_KEY'), env('MASTODON_INSTANCE_URL'));
+        $client = new MastodonAPI(env('MASTODON_API_KEY', ''), env('MASTODON_INSTANCE_URL', ''));
         $app->instance('App\Helper\MastodonAPI', $client);
     }
 }
